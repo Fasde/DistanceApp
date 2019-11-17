3e4b5632a87861fcc734280ce835cef0a802d8b4
Fasde <fabianpoker1@web.de>
2019-11-17 09:43:44 +0100

    DistanceApp V1.1 Added functionality for searching items

--- app/src/main/java/fasde/android/distanceapp/MainActivity.java
+++ app/src/main/java/fasde/android/distanceapp/MainActivity.java
@@ -3,13 +3,17 @@ package fasde.android.distanceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -18,6 +22,8 @@ import java.util.TreeMap;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    ArrayAdapter arrayAdapter;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listview);
+        editText = findViewById(R.id.inputSearch);
 
         List<String> setup = new ArrayList<>();
 
@@ -146,14 +153,33 @@ public class MainActivity extends AppCompatActivity {
 
         BigDecimal kosten;
         for (Map.Entry<String, Integer> entry : vereine.entrySet()) {
-            kosten = new BigDecimal(entry.getValue() * 0.30 * 2).setScale(2, RoundingMode.CEILING);
+            kosten = new BigDecimal(entry.getValue() * 0.30 * 2).setScale(2, RoundingMode.HALF_DOWN);
             setup.add(entry.getKey() + "\t\t||\t\t" + entry.getValue() + "km\t\t||\t\t" + kosten + "€");
         }
 
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, setup);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, setup);
 
         listView.setAdapter(arrayAdapter);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.this.arrayAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
     }
 
 
